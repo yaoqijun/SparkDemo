e571469f7136e8a09315547115c126de6ae8ee36
yaoqj <yaoqj@terminus.io>
2015-10-24 16:26:53 +0800

    add all

---
+++ src/main/java/org/yqj/spark/demo/TestDemo.java
@@ -0,0 +1,13 @@
+package org.yqj.spark.demo;
+
+/**
+ * Created by yaoqijun.
+ * Date:2015-10-24
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+public class TestDemo {
+    public static void main(String []args){
+        System.out.println("test");
+    }
+}
---
+++ src/test/org.yqj.spark.demo/TestTestDemo.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yaoqijun.
+ * Date:2015-10-24
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+public class TestTestDemo {
+}
