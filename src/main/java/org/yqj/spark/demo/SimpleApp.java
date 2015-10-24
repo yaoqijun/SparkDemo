479d2af64886eb034ba7901d34415b3e2cabcd41
yaoqj <yaoqj@terminus.io>
2015-10-24 18:04:32 +0800

    add all

---
+++ src/main/java/org/yqj/spark/demo/SimpleApp.java
@@ -0,0 +1,11 @@
+package org.yqj.spark.demo;
+
+/**
+ * Created by yaoqijun.
+ * Date:2015-10-24
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+public class SimpleApp {
+
+}
--- src/main/java/org/yqj/spark/demo/TestDemo.java
+++ src/main/java/org/yqj/spark/demo/TestDemo.java
@@ -1,5 +1,8 @@
 package org.yqj.spark.demo;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 /**
  * Created by yaoqijun.
  * Date:2015-10-24
@@ -8,6 +11,21 @@ package org.yqj.spark.demo;
  */
 public class TestDemo {
     public static void main(String []args){
-        System.out.println("test");
+        System.out.println("test java8");
+        Function<String,Integer> function = (t) -> Integer.valueOf(t);
+        System.out.println(function.apply("123"));
+
+        Function<String,Long> testHaha = (t) -> Long.valueOf(t);
+        System.out.println(testHaha.apply("123123"));
+
+        Predicate<Integer> predicate = (pam) -> {
+            if(pam > 0){
+                return true;
+            }else {
+                return false;
+            }
+        };
+        System.out.println(predicate.test(100));
+        System.out.println(predicate.test(-100));
     }
 }
