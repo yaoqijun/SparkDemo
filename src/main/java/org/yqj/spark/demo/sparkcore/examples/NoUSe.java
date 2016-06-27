9264f04c3e6afc63e803890354cdcd8d1b556911
yaoqijun <yaoqijunmail@foxmail.com>
2016-06-27 14:00:19 +0800

    fxi

---
+++ src/main/java/org/yqj/spark/demo/sparkcore/examples/NoUSe.java
@@ -0,0 +1,13 @@
+package org.yqj.spark.demo.sparkcore.examples;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-06-27
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+public class NoUse {
+    public static void main(String[] args) {
+        System.out.println("test examples context");
+    }
+}
